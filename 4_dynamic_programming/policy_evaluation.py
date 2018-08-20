1b44b7e995a796cd33d3ecf67a4b6251739e99bd
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2018-08-20 17:31:30 +0300

    Added state as key to the policy dictionary

--- 4_dynamic_programming/policy_evaluation.py
+++ 4_dynamic_programming/policy_evaluation.py
@@ -12,10 +12,11 @@ def main():
                              'down' : (1, 0),
                              'right' : (0, 1),
                              'left' : (0, -1)}
-    policy = {'up' : 0.25,
-              'down' : 0.25,
-              'right' : 0.25,
-              'left' : 0.25}  
+    policy = {}
+    for i in range(len(V)):
+        for j in range(len(V[i])):
+            for a in actions:
+                policy[(i, j, a)] = 0.25
     
     delta = theta + 1
     k = 0
@@ -35,8 +36,8 @@ def main():
                     operation = action_to_index_operation[a]
                     next_i = np.clip(i + operation[0], 0, 3)
                     next_j = np.clip(j + operation[1], 0, 3)
-#                    new_val += policy[a]*(reward + gamma*old_V[next_i][next_j])
-                    new_val += policy[a]*(reward + gamma*V[next_i][next_j])
+#                    new_val += policy[(i, j, a)]*(reward + gamma*old_V[next_i][next_j])
+                    new_val += policy[(i, j, a)]*(reward + gamma*V[next_i][next_j])
                 V[i][j] = new_val
                 delta = max(delta, abs(new_val - old_val))
                 
