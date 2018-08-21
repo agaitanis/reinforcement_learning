4146b1073a7d5f73f35ef0d28393abba5b11abef
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2018-08-21 22:51:52 +0300

    More generic approach to policy evaluation

--- 4_dynamic_programming/policy_evaluation.py
+++ 4_dynamic_programming/policy_evaluation.py
@@ -1,46 +1,61 @@
 import numpy as np
 
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
+
+
+def get_next_s(s, a):
+    if a == UP:
+        if s >= 4: return s - 4
+    elif a == DOWN:
+        if s < 12: return s + 4
+    elif a == RIGHT:
+        if (s + 1)%4 != 0: return s + 1
+    elif a == LEFT:
+        if s%4 != 0: return s - 1
+    return s
+
 
 def main():
     theta = 1e-6
+    actions_num = 4
+    states_num = 16
     gamma = 1
-    reward = -1
-    V = np.zeros((4, 4))
-    goals = [(0, 0), (3, 3)]
-    actions = ['up', 'down', 'right', 'left']
-    action_to_index_operation = {'up' : (-1, 0),
-                             'down' : (1, 0),
-                             'right' : (0, 1),
-                             'left' : (0, -1)}
-    policy = {}
-    for i in range(len(V)):
-        for j in range(len(V[i])):
-            for a in actions:
-                policy[(i, j, a)] = 0.25
+    V = np.zeros(states_num)
+    policy = np.zeros((states_num, actions_num))
+    P = np.zeros((states_num, actions_num, states_num))
+    R = np.full((states_num, actions_num, states_num), -1)
+
+    for s in range(1, states_num - 1):
+        for a in range(actions_num):
+            policy[s, a] = 0.25
     
+    for s in range(1, states_num - 1):
+        for a in range(actions_num):
+            next_s = get_next_s(s, a)
+            P[s, a, next_s] = 1
+
     delta = theta + 1
     k = 0
 
     while delta > theta:
         print("k =", k)
-        print(V)
+        print(V.reshape(4, 4))
         k += 1
         delta = 0
-#        old_V = np.copy(V)
-        for i in range(len(V)):
-            for j in range(len(V[i])):
-                if (i, j) in goals: continue
-                old_val = V[i][j]
-                new_val = 0
-                for a in actions:
-                    operation = action_to_index_operation[a]
-                    next_i = np.clip(i + operation[0], 0, 3)
-                    next_j = np.clip(j + operation[1], 0, 3)
-#                    new_val += policy[(i, j, a)]*(reward + gamma*old_V[next_i][next_j])
-                    new_val += policy[(i, j, a)]*(reward + gamma*V[next_i][next_j])
-                V[i][j] = new_val
-                delta = max(delta, abs(new_val - old_val))
-                
+        for s in range(states_num):
+            old_v = V[s]
+            new_v = 0
+            for a in range(actions_num):
+                temp_sum = 0
+                for next_s in range(states_num):
+                    temp_sum += P[s, a, next_s]*(R[s, a, next_s] + gamma*V[next_s])
+                new_v += policy[s, a]*temp_sum
+            V[s] = new_v
+            delta = max(delta, abs(new_v - old_v))
+
 
 if __name__ == '__main__':
     main()
