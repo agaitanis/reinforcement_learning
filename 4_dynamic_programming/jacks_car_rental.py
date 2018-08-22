69569c7f4c13658283a082d2a1f0c14575f66a51
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2018-08-22 23:54:37 +0300

    Fix for actions

--- 4_dynamic_programming/jacks_car_rental.py
+++ 4_dynamic_programming/jacks_car_rental.py
@@ -2,10 +2,7 @@ import numpy as np
 
 states_num1 = 21
 states_num2 = 21
-actions_num = 11
 
-def get_possible_actions(s1, s2):
-    pass
 
 def P(s1, s2, a, next_s1, next_s2):
     pass
@@ -13,14 +10,13 @@ def P(s1, s2, a, next_s1, next_s2):
 def R(s1, s2, a, next_s1, next_s2):
     pass
 
-def action_to_index(a):
-    return a + 5
 
 def main():
     theta = 1e-6
     gamma = 0.9
+#    actions = range(-5, 6)
     V = np.zeros(states_num1, states_num2)
-    policy = np.zeros(states_num1, states_num2, actions_num)
+    policy = np.zeros(states_num1, states_num2)
     delta = theta + 1
     k = 0
     while delta > theta:
@@ -32,16 +28,12 @@ def main():
             for s2 in range(states_num2):
                 old_v = V[s1, s2]
                 new_v = 0
-                actions = get_possible_actions(s1, s2)
-                for a in actions:
-                    temp_sum = 0
-                    for next_s1 in range(states_num1):
-                        for next_s2 in range(states_num2):
-                            temp_sum += P(s1, s2, a, next_s1, next_s2)
-                            *(R(s1, s2, a, next_s1, next_s2)
-                            + gamma*V[next_s1, next_s2])
-                    a_index = action_to_index(a)
-                    new_v += policy[s1, s2, a_index]*temp_sum
+                a = policy(s1, s2)
+                for next_s1 in range(states_num1):
+                    for next_s2 in range(states_num2):
+                        new_v += P(s1, s2, a, next_s1, next_s2)
+                        *(R(s1, s2, a, next_s1, next_s2)
+                        + gamma*V[next_s1, next_s2])
                 V[s1, s2] = new_v
                 delta = max(delta, abs(new_v - old_v))
 
