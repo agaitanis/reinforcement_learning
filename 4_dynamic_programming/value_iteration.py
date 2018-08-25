b4cc2079c0d640ea4402b7875ee237e5bbaffdca
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2018-08-25 14:57:01 +0300

    Gambler's Problem - Finished

--- 4_dynamic_programming/value_iteration.py
+++ 4_dynamic_programming/value_iteration.py
@@ -5,40 +5,50 @@ Reinforcement Learning by Sutton and Barto
 Example 4.3: Gambler's Problem
 '''
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
-    theta = 1e-18
-    p = 0.4
-    states_num = 101
+    theta = 1e-9
     gamma = 1
-    V = np.zeros(states_num)
-    R = np.zeros(states_num)
+    p = 0.4
+    V = np.zeros(101)
+    R = np.zeros(101)
     R[100] = 1
+    policy = np.zeros(101)
     
     delta = theta + 1
-    k = 0
+
+    fig1 = plt.figure()
+    ax1 = fig1.gca()
     
     while delta > theta:
-        print("k =", k)
-        print(V)
-        k += 1
         delta = 0
-        for s in range(1, states_num - 1):
+        for s in range(1, 100):
             v = V[s]
             q = []
             for a in range(1, min(s, 100 - s) + 1):
-                win_s = s + a
-                lose_s = s - a
-                next_states = (win_s, lose_s)
-                P = {win_s : p, lose_s : 1 - p}
-                q_a = 0
-                for next_s in next_states:
-                    q_a += P[next_s]*(R[next_s] + gamma*V[next_s])
-                q.append(q_a)
-            V[s] = max(q)
-            delta = max(delta, abs(v - V[s]))
-        
+                next_s_to_P = {s + a: p, s - a: 1 - p}
+                temp_sum = 0
+                for next_s, P in next_s_to_P.items():
+                    temp_sum += P*(R[next_s] + gamma*V[next_s])
+                q.append(temp_sum)
+            max_q = max(q)
+            V[s] = max_q
+            policy[s] = np.argwhere(abs(q - max_q) < 1e-6)[0] + 1
+            delta = max(delta, abs(v - V[s]))    
+        ax1.plot(range(1, 100), V[1:100])
+    plt.xlabel('Capital')
+    plt.ylabel('Value estimates')
+    plt.show()
+
+    fig2 = plt.figure()
+    ax2 = fig2.gca()
+    ax2.plot(range(1, 100), policy[1:100])
+    plt.xlabel('Capital')
+    plt.ylabel('Final policy (stake)')
+    plt.show()
+    
 
 if __name__ == '__main__':
     main()
