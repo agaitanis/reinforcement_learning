29bfce80cecc8614f0ae15c9dc0b4109c54baca3
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-02-06 23:16:06 +0200

    Minor, changed some variable names

--- 2_evaluative_feedback/n_armed_bandit_problem.py
+++ 2_evaluative_feedback/n_armed_bandit_problem.py
@@ -40,32 +40,32 @@ class Environment(object):
 def main():
     np.random.seed(0)
     n = 10
-    runs_num = 2000
-    episodes_num = 1000
+    tasks_num = 2000
+    plays_num = 1000
     eps = [0, 0.01, 0.1]
     
     f, (ax1, ax2) = plt.subplots(2, sharex=True)
 
     for i in range(len(eps)):
-        avg_rewards = np.zeros(episodes_num)
-        optimal_actions = np.zeros(episodes_num)
-        for j in range(runs_num):
+        avg_rewards = np.zeros(plays_num)
+        optimal_actions = np.zeros(plays_num)
+        for j in range(tasks_num):
             env = Environment(n)
             agent = Agent(n, eps[i])
             optimal_action = env.get_optimal_action()
-            for p in range(episodes_num):
+            for p in range(plays_num):
                 a = agent.select_action()
                 r = env.give_reward(a)
                 agent.accept_reward(a, r)
                 avg_rewards[p] += r
                 if a == optimal_action:
                     optimal_actions[p] += 1
-        episodes = range(1, episodes_num + 1)
-        optimal_actions = [100*x/runs_num for x in optimal_actions]
-        avg_rewards = [x/runs_num for x in avg_rewards]
+        plays = range(1, plays_num + 1)
+        optimal_actions = [100*x/tasks_num for x in optimal_actions]
+        avg_rewards = [x/tasks_num for x in avg_rewards]
         label = 'eps = ' + str(eps[i])
-        ax1.plot(episodes, avg_rewards, label=label)
-        ax2.plot(episodes, optimal_actions, label=label)
+        ax1.plot(plays, avg_rewards, label=label)
+        ax2.plot(plays, optimal_actions, label=label)
     
     ax1.set_xlabel('Plays')
     ax1.set_ylabel('Average Reward')
