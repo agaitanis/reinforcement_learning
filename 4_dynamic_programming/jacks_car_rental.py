ada1763fdec50b739a822af587edca87c4c02445
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-02-22 19:32:05 +0200

    Refactoring

--- 4_dynamic_programming/jacks_car_rental.py
+++ 4_dynamic_programming/jacks_car_rental.py
@@ -12,19 +12,6 @@ from matplotlib.ticker import MaxNLocator
 from mpl_toolkits.mplot3d import Axes3D
 
 
-MAX_CARS1 = 20
-MAX_CARS2 = 20
-LAMBDA_RENTALS1 = 3
-LAMBDA_RENTALS2 = 4
-LAMBDA_RETURNS1 = 3
-LAMBDA_RETURNS2 = 2
-POISSON_POINTS_NUM = 20
-RENTAL_REWARD = 10
-MOVE_COST = 2
-THETA = 1e-6
-GAMMA = 0.9
-
-
 def poisson(n, l):
     return math.pow(l, n)*math.exp(-l)/math.factorial(n)
 
@@ -37,16 +24,17 @@ def index_to_a(a_i):
     return a_i - 5
 
 
-def calc_probs_and_rewards(cur_s, max_cars, lambda_rentals, lambda_returns):
+def calc_probs_and_rewards(cur_s, max_cars, lambda_rentals, lambda_returns,
+                           poisson_points_num, rental_reward):
     states_num = max_cars + 1
     p = np.zeros(states_num)
     r = np.zeros(states_num)
     
-    for rentals in range(POISSON_POINTS_NUM):
+    for rentals in range(poisson_points_num):
         p1 = poisson(rentals, lambda_rentals)
         rentals = min(rentals, cur_s)
-        reward = RENTAL_REWARD*rentals         
-        for returns in range(POISSON_POINTS_NUM):
+        reward = rental_reward*rentals         
+        for returns in range(poisson_points_num):
             p2 = poisson(returns, lambda_returns)
             next_s = min(cur_s - rentals + returns, max_cars)
             p[next_s] += p1*p2
@@ -59,15 +47,15 @@ def calc_probs_and_rewards(cur_s, max_cars, lambda_rentals, lambda_returns):
     return p, r
 
 
-def plot_V(V):
+def plot_V(V, max_cars1, max_cars2):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    states1 = range(MAX_CARS1 + 1)
-    states2 = range(MAX_CARS2 + 1)
+    states1 = range(max_cars1 + 1)
+    states2 = range(max_cars2 + 1)
     states1, states2 = np.meshgrid(states1, states2)
     ax.plot_surface(states1, states2, V, cmap=cm.coolwarm)
-    ax.set_xlim(0, MAX_CARS1)
-    ax.set_ylim(0, MAX_CARS2)
+    ax.set_xlim(0, max_cars1)
+    ax.set_ylim(0, max_cars2)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
     ax.set_title('V')
@@ -84,74 +72,113 @@ def plot_policy(policy):
     ax.invert_yaxis()
     ax.set_title('π')
     plt.show()
-    
 
-def main():
-    states_num1 = MAX_CARS1 + 1
-    states_num2 = MAX_CARS2 + 1
-    actions = range(-5, 6)
-    V = np.zeros((states_num1, states_num2))
-    policy = np.zeros((states_num1, states_num2), dtype=int)
-    P = np.zeros((states_num1, states_num2, len(actions), states_num1, states_num2))
-    R = np.zeros((states_num1, states_num2, len(actions), states_num1, states_num2))
+
+class Environment(object):
+    def __init__(self, max_cars1, max_cars2, actions,
+                 lambda_rentals1, lambda_rentals2,
+                 lambda_returns1, lambda_returns2,
+                 poisson_points_num, rental_reward,
+                 move_cost):
+        states_num1 = max_cars1 + 1
+        states_num2 = max_cars2 + 1
+        self.P = np.zeros((states_num1, states_num2, len(actions), states_num1, states_num2))
+        self.R = np.zeros((states_num1, states_num2, len(actions), states_num1, states_num2))
     
-    for s1 in range(states_num1):
-        for s2 in range(states_num2):
-            for a in actions:
-                if a > 0 and a > s1: continue
-                if a < 0 and -a > s2: continue
-                a_i = a_to_index(a)
-                cur_s1 = min(s1 - a, MAX_CARS1)
-                cur_s2 = min(s2 + a, MAX_CARS2)
-                p1, r1 = calc_probs_and_rewards(cur_s1, MAX_CARS1, LAMBDA_RENTALS1, LAMBDA_RETURNS1)
-                p2, r2 = calc_probs_and_rewards(cur_s2, MAX_CARS2, LAMBDA_RENTALS2, LAMBDA_RETURNS2)
-                for next_s1 in range(states_num1):
-                    for next_s2 in range(states_num2):
-                        P[s1, s2, a_i, next_s1, next_s2] = p1[next_s1]*p2[next_s2]
-                        R[s1, s2, a_i, next_s1, next_s2] = r1[next_s1] + r2[next_s2] - MOVE_COST*abs(a)
-    
-    policy_stable = False
-    
-    while not policy_stable:
-        delta = THETA + 1
-        k = 0
-        while delta > THETA:
-            k += 1
-            delta = 0
-            for s1 in range(states_num1):
-                for s2 in range(states_num2):
-                    old_v = V[s1, s2]
-                    new_v = 0
-                    a = policy[s1, s2]
-                    a_i = a_to_index(a)
-                    for next_s1 in range(states_num1):
-                        for next_s2 in range(states_num2):
-                            new_v += P[s1, s2, a_i, next_s1, next_s2]*\
-                            (R[s1, s2, a_i, next_s1, next_s2] + GAMMA*V[next_s1, next_s2])
-                    V[s1, s2] = new_v
-                    delta = max(delta, abs(new_v - old_v))
-        
-        policy_stable = True
-        
         for s1 in range(states_num1):
             for s2 in range(states_num2):
-                q = []
-                b = policy[s1, s2]
                 for a in actions:
+                    if a > 0 and a > s1: continue
+                    if a < 0 and -a > s2: continue
                     a_i = a_to_index(a)
-                    temp_sum = 0
+                    cur_s1 = min(s1 - a, max_cars1)
+                    cur_s2 = min(s2 + a, max_cars2)
+                    p1, r1 = calc_probs_and_rewards(cur_s1, max_cars1,
+                                                    lambda_rentals1, lambda_returns1,
+                                                    poisson_points_num, rental_reward)
+                    p2, r2 = calc_probs_and_rewards(cur_s2, max_cars2,
+                                                    lambda_rentals2, lambda_returns2,
+                                                    poisson_points_num, rental_reward)
                     for next_s1 in range(states_num1):
                         for next_s2 in range(states_num2):
-                            temp_sum += P[s1, s2, a_i, next_s1, next_s2]*\
-                            (R[s1, s2, a_i, next_s1, next_s2] + GAMMA*V[next_s1, next_s2])
-                    q.append(temp_sum)
-                best_a_i = np.argmax(q)
-                policy[s1, s2] = index_to_a(best_a_i)
-                if b != policy[s1, s2]:
-                    policy_stable = False
+                            self.P[s1, s2, a_i, next_s1, next_s2] = p1[next_s1]*p2[next_s2]
+                            self.R[s1, s2, a_i, next_s1, next_s2] = r1[next_s1] + r2[next_s2] - move_cost*abs(a)
 
-    plot_V(V)
-    plot_policy(policy)
+
+class Agent(object):
+    def __init__(self, max_cars1, max_cars2, actions, gamma, theta, P, R):
+        self.states_num1 = max_cars1 + 1
+        self.states_num2 = max_cars2 + 1
+        self.actions = actions
+        self.gamma = gamma
+        self.theta = theta
+        self.P = P
+        self.R = R
+        self.V = np.zeros((self.states_num1, self.states_num2))
+        self.policy = np.zeros((self.states_num1, self.states_num2), dtype=int)
+
+    
+    def evaluate_policy(self):
+        delta = self.theta + 1
+        
+        while delta > self.theta:
+            delta = 0
+            for s1 in range(self.states_num1):
+                for s2 in range(self.states_num2):
+                    old_v = self.V[s1, s2]
+                    new_v = 0
+                    a = self.policy[s1, s2]
+                    a_i = a_to_index(a)
+                    for next_s1 in range(self.states_num1):
+                        for next_s2 in range(self.states_num2):
+                            new_v += self.P[s1, s2, a_i, next_s1, next_s2]*\
+                            (self.R[s1, s2, a_i, next_s1, next_s2] + self.gamma*self.V[next_s1, next_s2])
+                    self.V[s1, s2] = new_v
+                    delta = max(delta, abs(new_v - old_v))
+
+
+    def improve_policy(self):
+        policy_stable = False
+        
+        while not policy_stable:
+            self.evaluate_policy()
+            
+            policy_stable = True
+            
+            for s1 in range(self.states_num1):
+                for s2 in range(self.states_num2):
+                    q = []
+                    b = self.policy[s1, s2]
+                    for a in self.actions:
+                        a_i = a_to_index(a)
+                        temp_sum = 0
+                        for next_s1 in range(self.states_num1):
+                            for next_s2 in range(self.states_num2):
+                                temp_sum += self.P[s1, s2, a_i, next_s1, next_s2]*\
+                                (self.R[s1, s2, a_i, next_s1, next_s2] + self.gamma*self.V[next_s1, next_s2])
+                        q.append(temp_sum)
+                    best_a_i = np.argmax(q)
+                    self.policy[s1, s2] = index_to_a(best_a_i)
+                    if b != self.policy[s1, s2]:
+                        policy_stable = False
+
+
+def main():
+    max_cars1 = 20
+    max_cars2 = 20
+    actions = range(-5, 6)
+    env = Environment(max_cars1, max_cars2, actions,
+                      lambda_rentals1=3, lambda_rentals2=4,
+                      lambda_returns1=3, lambda_returns2=2,
+                      poisson_points_num=20, rental_reward=10,
+                      move_cost=2)
+    agent = Agent(max_cars1, max_cars2, actions,
+                  gamma=0.9, theta=1e-6, P=env.P, R=env.R)
+    
+    agent.improve_policy()
+
+    plot_V(agent.V, max_cars1, max_cars2)
+    plot_policy(agent.policy)
 
     
 
