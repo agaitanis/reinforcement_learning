2f2f012ab3f6d2236f1f62fc133643d81c2e6ca2
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-03-04 08:55:43 +0200

    Refactoring

--- 6_temporal_difference_learning/windy_gridworld.py
+++ 6_temporal_difference_learning/windy_gridworld.py
@@ -4,10 +4,21 @@ Reinforcement Learning by Sutton and Barto
 6. Temporal-Difference Learning
 6.4 Sarsa: On-Policy TD Control
 Example 6.5: Windy Gridworld
+Exercise 6.6: Windy Gridworld with King's Moves
 '''
 import numpy as np
 from collections import defaultdict
 import matplotlib.pyplot as plt
+
+
+def print_greedy_policy(Q, grid_size, action_to_str):
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            s = (i, j)
+            a = np.argmax(Q[s])
+            print(action_to_str[a], end=' ')
+        print()
+    print()
 
 
 class Environment(object):
@@ -51,41 +62,61 @@ class Agent(object):
         return self.action_to_move[a]
 
 
-    def print_greedy_policy(self, grid_size):
-        for i in range(grid_size[0]):
-            for j in range(grid_size[1]):
-                s = (i, j)
-                a = np.argmax(self.Q[s])
-                print(self.action_to_str[a], end=' ')
-            print()
-        print()    
+class AgentKing(object):
+    def __init__(self, alpha, gamma, eps):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.eps = eps
+        self.action_to_move = ((-1, 0), (1, 0), (0, 1), (0, -1),
+                               (-1, 1), (-1, -1), (1, 1), (1, -1))
+        self.action_to_str = ('u ', 'd ', 'r ', 'l ',
+                              'ur', 'ul', 'dr', 'dl')
+        self.actions = range(len(self.action_to_move))
+        self.Q = defaultdict(lambda: np.zeros(len(self.actions)))
+
+
+    def select_action(self, s):
+        if np.random.uniform() < self.eps:
+            return np.random.choice(self.actions)
+        else:
+            return np.argmax(self.Q[s])
+
+
+    def improve_policy(self, s, a, r, new_s, new_a):
+        self.Q[s][a] += self.alpha*(r + self.gamma*self.Q[new_s][new_a] - self.Q[s][a])
+
+    
+    def convert_action_to_move(self, a):
+        return self.action_to_move[a]  
 
 
 def main():
-    np.random.seed(0)
     env = Environment()
-    agent = Agent(alpha=0.4, gamma=1.0, eps=0.1)  
-    episodes_num = 170
-    steps = 0
-    episodes = []
-    
-    for i in range(episodes_num):
-        s = env.start_state
-        a = agent.select_action(s)
-        while s != env.end_state:
-            move = agent.convert_action_to_move(a)
-            r, new_s = env.respond_to_move(s, move)
-            new_a = agent.select_action(new_s)
-            agent.improve_policy(s, a, r, new_s, new_a)
-            s, a = new_s, new_a
-            steps += 1
-            episodes.append(i)
-            
-    agent.print_greedy_policy(env.grid_size)
-    plt.plot(range(steps), episodes)
-    plt.xlabel("Time steps")
-    plt.ylabel("Episodes")
-    
+    agent1 = Agent(alpha=0.4, gamma=1.0, eps=0.1)
+    agent2 = AgentKing(alpha=0.5, gamma=1.0, eps=0.1)
+    agents = (agent1, agent2)
+    episodes_num = (170, 200)
+
+    for i, agent in enumerate(agents):
+        np.random.seed(0)
+        steps = 0
+        episodes = []
+        for i in range(episodes_num[i]):
+            s = env.start_state
+            a = agent.select_action(s)
+            while s != env.end_state:
+                move = agent.convert_action_to_move(a)
+                r, new_s = env.respond_to_move(s, move)
+                new_a = agent.select_action(new_s)
+                agent.improve_policy(s, a, r, new_s, new_a)
+                s, a = new_s, new_a
+                steps += 1
+                episodes.append(i)            
+        print_greedy_policy(agent.Q, env.grid_size, agent.action_to_str)
+        if agent is agent1:
+            plt.plot(range(steps), episodes)
+            plt.xlabel("Time steps")
+            plt.ylabel("Episodes")
 
 
 if __name__ == "__main__":
