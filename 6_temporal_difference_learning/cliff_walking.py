9239f7794f0b689db2e4560d26760ac1985a43ad
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2019-03-05 22:08:49 +0200

    Refactoring

--- 6_temporal_difference_learning/cliff_walking.py
+++ 6_temporal_difference_learning/cliff_walking.py
@@ -7,87 +7,122 @@ Example 6.6: Cliff Walking
 """
 import numpy as np
 from collections import defaultdict
-import matplotlib.pyplot as plt
-import scipy
 
 
-class CliffWalking:
+def print_greedy_policy(name, Q, grid_size, action_to_str):
+    print(name)
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            s = (i, j)
+            a = np.argmax(Q[s])
+            print(action_to_str[a], end=' ')
+        print()
+    print()
+
+
+class Environment(object):
     def __init__(self):
-        self.eps = 0.1
-        self.alpha = 0.5
-        self.gamma = 1
-        self.episodes_num = 500
         self.grid_size = (4, 12)
         self.start_state = (3, 0)
         self.end_state = (3, 11)
         self.cliff = [(3, x) for x in range(1, 11)]
-        self.moves = ((-1, 0), (1, 0), (0, 1), (0, -1))
-        self.action_str = ('u', 'd', 'r', 'l')
-        self.actions = range(len(self.moves))
 
 
-    def print_greedy_policy(self, Q):
-        for i in range(self.grid_size[0]):
-            for j in range(self.grid_size[1]):
-                s = (i, j)
-                a = np.argmax(Q[s])
-                print(self.action_str[a], end=' ')
-            print()
-        print()
-
-    
-    def get_next_action(self, s, Q):
-        if np.random.uniform() < self.eps:
-            return np.random.choice(self.actions)
-        else:
-            return np.argmax(Q[s])
-
-
-    def get_next_state(self, s, a):
-        new_s = tuple(np.clip(np.array(s) + np.array(self.moves[a]), (0, 0), self.grid_size))
+    def respond_to_move(self, s, move):
+        new_s = tuple(np.clip(np.array(s) + np.array(move), (0, 0), self.grid_size))
         if new_s in self.cliff:
             return self.start_state, -100
         else:
             return tuple(new_s), -1
 
+
+class AgentQ(object):
+    def __init__(self, alpha, gamma, eps):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.eps = eps
+        self.action_to_move = ((-1, 0), (1, 0), (0, 1), (0, -1))
+        self.action_to_str = ('u', 'd', 'r', 'l')
+        self.actions = range(len(self.action_to_move))
+        self.Q = defaultdict(lambda: np.zeros(len(self.actions)))
+
     
-    def q_learning(self):
-        np.random.seed(0)
-        Q = defaultdict(lambda: len(self.actions)*[0])
+    def select_action(self, s):
+        if np.random.uniform() < self.eps:
+            return np.random.choice(self.actions)
+        else:
+            return np.argmax(self.Q[s])
+    
+    
+    def improve_policy(self, s, a, r, new_s):
+        self.Q[s][a] += self.alpha*(r + self.gamma*max(self.Q[new_s]) - self.Q[s][a])
+    
+    
+def q_learning(episodes_num, alpha, gamma, eps):
+    np.random.seed(0)
+    env = Environment()
+    agent = AgentQ(alpha, gamma, eps)
 
-        for i in range(self.episodes_num):
-            s = self.start_state
-            while s != self.end_state:
-                a = self.get_next_action(s, Q)
-                new_s, r = self.get_next_state(s, a)
-                Q[s][a] += self.alpha*(r + self.gamma*max(Q[new_s]) - Q[s][a])
-                s = new_s
-        print("Q-Learning")
-        self.print_greedy_policy(Q)
+    for i in range(episodes_num):
+        s = env.start_state
+        while s != env.end_state:
+            a = agent.select_action(s)
+            move = agent.action_to_move[a]
+            new_s, r = env.respond_to_move(s, move)
+            agent.improve_policy(s, a, r, new_s)
+            s = new_s
+
+    print_greedy_policy("Q-Learning", agent.Q, env.grid_size, agent.action_to_str)
 
 
-    def sarsa(self):
-        np.random.seed(0)
-        Q = defaultdict(lambda: len(self.actions)*[0])
+class AgentSarsa(object):
+    def __init__(self, alpha, gamma, eps):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.eps = eps
+        self.action_to_move = ((-1, 0), (1, 0), (0, 1), (0, -1))
+        self.action_to_str = ('u', 'd', 'r', 'l')
+        self.actions = range(len(self.action_to_move))
+        self.Q = defaultdict(lambda: np.zeros(len(self.actions)))
+    
 
-        for i in range(self.episodes_num):
-            s = self.start_state
-            a = self.get_next_action(s, Q)
-            while s != self.end_state:
-                new_s, r = self.get_next_state(s, a)
-                new_a = self.get_next_action(new_s, Q)
-                Q[s][a] += self.alpha*(r + self.gamma*Q[new_s][new_a] - Q[s][a])
-                s = new_s
-                a = new_a
-        print("Sarsa")
-        self.print_greedy_policy(Q)
+    def select_action(self, s):
+        if np.random.uniform() < self.eps:
+            return np.random.choice(self.actions)
+        else:
+            return np.argmax(self.Q[s])
+    
+    
+    def improve_policy(self, s, a, r, new_s, new_a):
+        self.Q[s][a] += self.alpha*(r + self.gamma*self.Q[new_s][new_a] - self.Q[s][a])
+
+
+def sarsa(episodes_num, alpha, gamma, eps):
+    np.random.seed(0)
+    env = Environment()
+    agent = AgentSarsa(alpha, gamma, eps)
+
+    for i in range(episodes_num):
+        s = env.start_state
+        a = agent.select_action(s)
+        while s != env.end_state:
+            move = agent.action_to_move[a]
+            new_s, r = env.respond_to_move(s, move)
+            new_a = agent.select_action(new_s)
+            agent.improve_policy(s, a, r, new_s, new_a)
+            s, a, = new_s, new_a
+
+    print_greedy_policy("Sarsa", agent.Q, env.grid_size, agent.action_to_str)
 
 
 def main():
-    cw = CliffWalking()   
-    cw.q_learning()
-    cw.sarsa()
-
+    episodes_num = 500
+    alpha = 0.5
+    gamma = 1.0
+    eps = 0.1
+    q_learning(episodes_num, alpha, gamma, eps)
+    sarsa(episodes_num, alpha, gamma, eps)
+    
 
 if __name__ == "__main__":
     main()
